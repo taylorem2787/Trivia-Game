47260401cd0e4c066090a11f49270d34ec8f65d6
Taylor Emolo <tayloremolo@tay.local>
2016-09-08 15:17:56 -0400

    Prototype

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,6 @@
-// You'll create a trivia game that shows only one question 
-//until the player answers it or their time runs out.
-
-// If the player selects the correct answer, 
-//show a screen congratulating them for choosing the right option. 
-//After a few seconds, display the next question -- do this without user input.
-
-// The scenario is similar for wrong answers and time-outs.
-
-// If the player runs out of time, 
-//tell the player that time's up and display the correct answer. 
-//Wait a few seconds, then show the next question.
-// If the player chooses the wrong answer, 
-//tell the player they selected the wrong option and then display the correct answer. 
-//Wait a few seconds, then show the next question.
-// On the final screen, show the number of correct answers, 
-//incorrect answers, and an option to restart the game (without reloading the page).
-
 //USER PRESSES BUTTON START TO BEGIN GAME
 //1ST ROUND - QUESTION APPEARS ON SCREEN W/ MULTIPLE CHOICE OPTIONS 
-//TIMER COUNTS DOWN - 20 SECONDS
+//timeLeft COUNTS DOWN - 20 SECONDS
 //USER DOES ONE OF 3 THINGS:
 //	1.USER ANSWERS CORRECT 
 //	2.USER ANSWERS QUESTIONS WRONG 
@@ -37,249 +19,189 @@
 //DETERMINES PLAYER'S FINAL SCORE
 //RESTART/RESET GAME
 
-//GLOBAL VARIABLES
+//VARIABLES
 //========================================================================
-// var questions = "";
-// var multipleChoice = [];
-// var rounds = 
-// var counter = 1000 * 20;
-// var timer = 0
 $(document).ready(function(){
-  
+
+    var questions = [{
+                    question: "\"Why so serious?\"",
+                    choices: ["Suicide Squad", "Batman Begins", "The Dark Knight"],
+                    answer: 2
+                    },{
+                    question: "\"Could you describe the ruckus, Sir?\"",
+                    choices: ["The Breakfast Club", "16 Candles", "Scary Movie"],
+                    answer: 1
+                    },{
+                    question: "\"You talkin\' to me?\"",
+                    choices: ["Taxi Driver", "Scarface", "The Godfather"],
+                    answer: 1
+                    },{
+                    question: "\"I keep getting older...they stay the same age\"",
+                    choices: ["Empire Records", "Dazed and Confused", "How High"],
+                    answer: 2
+                    },{
+                    question: "\“I’m having an old friend for dinner\”",
+                    choices: ["Psycho", "Hannibal", "Silence of The Lambs"],
+                    answer: 1
+                    },{
+                    question: "\"I will look for you, I will find you, and I will kill you.\"",
+                    choices: ["Taken", "Terminator 2", "Terminator"],
+                    answer: 1
+                    },{
+                    question: "\"Ma!!! The meatloaf!!!\"",
+                    choices: ["Wedding Crashers", "The Wedding Singer", "Old Scool"],
+                    answer: 1
+                    }];
 
 
-var slideshowIntervalId = 0;
-var displayEachQuestion = 0;
+    $('#testDiv').hide();
+    $('img#bm').hide();   
 
-var gameHolder = {
-     count: 0,
-     wins: 0,
-     losses: 0,
-     round: 1, 
-     corectAnswer: [],
-     //gameOver = false;
-     //nextRound = false;
+    var counter = 0;
+    var round = 0;
+    var correct = 0;
+    var wrong = 0;
+    var correctArray = [];
+    var wrongArray = [];
 
-    questions: {
- 
-        "slideOne": {
-            "question": "question1",
-            "options":    
-                [
-                    { 
-                    "optionOne": "option1"
-                },
-                    {
-                    "optionTwo": "option2"
-                },
-                   { 
-                    "optionThree": "option3"
-                },
-                    {
-                    "optionFour": "option4"
-                }
-                ]
-        }, 
+    function displayQuestion(index){
+        var currentQ = $('<div id="questionDiv">');
+        var currentP = $('<p>');
+        currentP.append(questions[index].question);
+        currentQ.append(currentP);
+        return currentQ;
+    };
 
-        "slideTwo": {
-            "question": "question2",
-            "options":    
-                [
-                    { 
-                    "optionOne": "option1"
-                },
-                    {
-                    "optionTwo": "option2"
-                },
-                   { 
-                    "optionThree": "option3"
-                },
-                    {
-                    "optionFour": "option4"
-                }
-                ]
-        }, 
+    function displayFirst(index){
+        var newDiv = $('<div id=answer1>');
+        var answer1 = $('<p>');
+        answer1.text(questions[index].choices[0]);
+        newDiv.append(answer1)
+        return newDiv;
+    };
 
-        "slideThree": {
-            "question": "question3",
-            "options":    
-                [
-                    { 
-                    "optionOne": "option1"
-                },
-                    {
-                    "optionTwo": "option2"
-                },
-                   { 
-                    "optionThree": "option3"
-                },
-                    {
-                    "optionFour": "option4"
-                }
-                ]
-        },   
+    function displaySecond(index){
+        var newDiv = $('<div id=answer2>');
+        var answer2 = $('<p>');
+        answer2.text(questions[index].choices[1]);
+        newDiv.append(answer2);
+        return newDiv;
+    };
 
-        "slideFour": {    
-            "question": "question4",
-            "options":    
-                [
-                    { 
-                    "optionOne": "option1"
-                },
-                    {
-                    "optionTwo": "option2"
-                },
-                   { 
-                    "optionThree": "option3"
-                },
-                    {
-                    "optionFour": "option4"
-                }
-                ]
+        function displayThird(index){
+        var newDiv = $('<div id=answer3>');
+        var answer3 = $('<p>');
+        answer3.text(questions[index].choices[2]);
+        newDiv.append(answer3);
+        return newDiv;
+    };
+
+    function nextQuestion(){
+
+        if (counter < questions.length){
+            //$(".char").remove();
+            $('#questionDiv').remove();
+            $('#answer1').remove();
+            $('#answer2').remove();
+            $('#answer3').remove();
+
+
+            var next = displayQuestion(counter);
+            $('#questionDisplay').append(next);
+
+            var firstQ = displayFirst(counter);
+            $('#firstQ').append(firstQ);
+
+            var secondQ = displaySecond(counter);
+            $('#secondQ').append(secondQ);
+
+            var thirdQ = displayThird(counter);
+            $('#thirdQ').append(thirdQ);
+            } else {
+            $('#testDiv').hide();
+            $('#start').hide();
+            $('#finalAnswer').text("You got " + correctArray.length + " correct");
+            $('img#bm').show();
         }
-    }
-}
-  
+    };
 
-window.onload = function(){
-    $('#lap').on('click', stopwatch.recordLap);
-    $('#stop').on('click', stopwatch.stop);
-    $('#reset').on('click', stopwatch.reset);
-    $('#start').on('click', stopwatch.start);
-};
+    function choice(){
+       var answer = document.getElementsByName('q');
+        for(i = 0; i < answer.length; i++){
+          if (answer[i].checked){
+            if (answer[i].value == questions[counter].answer.length){
+                correct++;
+                correctArray.push(questions[counter].answer);
+                $(".winCounter").html("correct: " + correct);
+                round++;
+                $(".roundCounter").html("Round: " + round);
+                break;
+                } //2nd if
+            
 
+             else if (answer[i].value !== questions[counter].answer){
+                wrong++;
+                $(".lossCounter").html("Wrong: " + wrong);
+                wrongArray.push(questions[counter].answer);
+                round++;
+                $(".roundCounter").html("Round: " + round);
+                break;
+                }
+            } 
+        }   
+    }; //CHOICE
 
-var stopwatch = {
-    time:0,
-    lap:1,
-    reset: function(){
-        stopwatch.time = 0;
-        stopwatch.lap = 1;
-        $('#display').html('00:00');
-        $('#laps').html('');
-    },
-    start: function(){
-        counter = setInterval(stopwatch.count, 1000);
-    },
-    stop: function(){
-        clearInterval(counter);
-    },
-    recordLap: function(){
-        //if you use "this" here, it will point to the lap button, not this object.
-        var converted = stopwatch.timeConverter(stopwatch.time);
-        $('#laps').append('<p>Lap ' + stopwatch.lap + ' : ' + converted + '</p>');
-        stopwatch.lap++;
-    },
-    count: function(){
-        stopwatch.time++;
-        var converted = stopwatch.timeConverter(stopwatch.time);
-        $('#display').html(converted);
-    },
-    timeConverter: function(t){
-        var minutes = Math.floor(t/60);
-        var seconds = t - (minutes * 60);
-        if (seconds < 10){
-            seconds = "0" + seconds;
-        }
-        if (minutes === 0){
-            minutes = "00";
-        } else if (minutes < 10){
-            minutes = "0" + minutes;
-        }
-        return minutes + ":" + seconds;
-    }
-};
+    $('#start').on('click', function(){
+        $('#testDiv').show();
+        $('#start').hide();
+        nextQuestion();
+    });
 
+    $('#next').on('click', function(){
+        choice();
+        counter++;
+        nextQuestion();
+    });
+//STOP WATCH
+    var stopwatch = {
+          number: 15,
+          run: function() {
+            count = setInterval(stopwatch.decrement, 1000);
+          },
+          decrement: function(){
+            stopwatch.number--;
+            $('#show-number').html('<h2>' + stopwatch.number + '<h2>');
+                if (stopwatch.number === 0){
+                    choice();
+                    counter++;
+                    nextQuestion();
+                    stopwatch.clear();
+                    stopwatch.resetCounter();
+                    stopwatch.run();
+                }   
+          },
+        resetCounter: function(){
+                stopwatch.number = 16;
+          },
+        clear: function(){
+                clearInterval(count);
+          }
+    };
 
+    $('#start').on('click', function(){
+        $('#testDiv').show();
+        $('#start').hide();
+        nextQuestion();
+        stopwatch.run();
+    });
 
-    //Use jQuery to run "startSlideshow" when you click the "start" button
-        // $("#start").on("click", startSlideshow);
-        $("#start").on("click", displayQuestion);
+    $('#next').on('click', function(){
+        choice();
+        counter++;
+        nextQuestion();
+        stopwatch.clear();
+        stopwatch.resetCounter();
+        stopwatch.run();
+    }); //STOPWATCH END
+})
 
-    //Use jQuery to run "stopSlideshow" when you click the "stop" button
-        $("#stop").on("click", stopSlideshow);
-
-
-    function displayQuestion (){
-        var keys = Object.keys(gameHolder.questions);
-            $('.question').html(gameHolder.questions[keys[gameHolder.count]].question);
-            $('.optionOne').html(gameHolder.questions[keys[gameHolder.count]].options[0].optionOne);
-            $('.optionTwo').html(gameHolder.questions[keys[gameHolder.count]].options[1].optionTwo);
-            $('.optionThree').html(gameHolder.questions[keys[gameHolder.count]].options[2].optionThree);
-            $('.optionFour').html(gameHolder.questions[keys[gameHolder.count]].options[3].optionFour);
-
-
-                // if (user == gameholder.questions.question.options.answers){
-                //     gameholder.wins++;
-                // }
-
-                // else (user == options);
-                // gameHolder.losses++ 
-
-            //console.log("displayQuestion");
-           console.log("running displayQuestion");
-    }
-
-    
-
-    // $(".options").on("click", function(){
-    //     gameHolder.losses++;
-    //     //gameHolder.corectAnswer === gameHolder.wins;
-    //     $(".lossCounter").html("Losses: " +gameHolder.losses);
-    //     gameHolder.round++;
-    //     $(".roundCounter").html("Round: " +gameHolder.round);
-
-    //     gameHolder.nextRound == true;
-    // });
-
-    // $(".a1").on("click", function(){
-    //     gameHolder.corectAnswer === gameHolder.wins;
-    //     gameHolder.wins++;
-    //     $(".winCounter").html("Wins: " +gameHolder.wins);
-    //     gameHolder.round++;
-    //     $(".roundCounter").html("Round: " +gameHolder.round);
-    //     alert('right!');
-    //     gameHolder.nextRound == true;
-    //  });
-
-    function nextQuestion(){   //increment the count by 1
-        gameHolder.questions.count++;
-        console.log("count: " + gameHolder.count);
-        //if the count is the same as the length of the image array, reset the count to 0
-            if (gameHolder.questions.count === gameHolder.questions.length) {
-            gameHolder.questions.count = 0;
-
-            console.log("running displayQuestion");
-    }
-
-
-
-        displayQuestion();     //show "image-holder" div
-}
-
-
-    function startSlideshow (){     //use a setInterval to run nextImage
-        console.log("start slideshow");
-        slideshowIntervalId = setInterval(nextImage, 1000 * 5);
-}
-
-function stopSlideshow () {     //put your clearInterval here:
-    clearInterval(slideshowIntervalId);
-
-
-   console.log("running displayQuestion");
-}
-
-}) //end of document.ready
-
-
-
-
-
-
-//FUNCTIONS
-//========================================================================
-
-
-//MAIN PROCESS
-//========================================================================
